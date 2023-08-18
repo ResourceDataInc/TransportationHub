1372e02d2eb22f87f14de61cc2c789bf36c3f5c7
brandonkirlin <bkirlin@resourcedata.com>
2023-08-18 16:33:33 -0700

    Update Routes definition

---
+++ Databases/TRANSPORTATION_HUB/HUB/Tables/ROUTES.sql
@@ -0,0 +1,12 @@
+CREATE  TABLE TRANSPORTATION_HUB.HUB.ROUTES ( 
+	ROUTE_ID             number(38,0) NOT NULL    ,
+	ROUTE_TYPE           varchar(10)     ,
+	ROUTE_SUB_TYPE       varchar(20)     ,
+	ROUTE_COLOR          varchar(6)     ,
+	FREQUENT_SERVICE     boolean     ,
+	ROUTE_DESC           varchar(50)     ,
+	CONSTRAINT UNQ_ROUTE_ROUTE_ID UNIQUE ( ROUTE_ID ),
+	CONSTRAINT PK_ROUTE_ROUTE_ID PRIMARY KEY ( ROUTE_ID )
+ );
+
+COMMENT ON COLUMN TRANSPORTATION_HUB.HUB.ROUTES.ROUTE_ID IS '"routeNumber" in JSON dataset';
