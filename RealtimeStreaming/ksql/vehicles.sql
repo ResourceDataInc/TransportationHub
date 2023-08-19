b69035b5addf3bd115ee9e49ad0fb661357cc742
Akira Madono <amadono@resourcedata.com>
2023-08-18 17:06:20 -0700

    working version all the way to end table

---
+++ RealtimeStreaming/ksql/vehicles.sql
@@ -0,0 +1,5 @@
+CREATE STREAM vehicles
+  WITH (KAFKA_TOPIC='VehiclePositions',
+        KEY_FORMAT = 'KAFKA',
+        VALUE_FORMAT='PROTOBUF',
+        VALUE_SCHEMA_FULL_NAME='transit_realtime.FeedMessage');
