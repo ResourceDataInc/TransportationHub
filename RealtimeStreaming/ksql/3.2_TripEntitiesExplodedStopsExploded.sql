54070aab226ea6993f88811f499f7fb5ca1ade56
Akira Madono <amadono@resourcedata.com>
2023-08-29 13:28:42 -0700

    working ksql flow

---
+++ RealtimeStreaming/ksql/3.2_TripEntitiesExplodedStopsExploded.sql
@@ -0,0 +1,10 @@
+CREATE STREAM TripEntitiesExplodedStopsExploded 
+  WITH (KAFKA_TOPIC='TripEntitiesExplodedStopsExploded', VALUE_FORMAT='PROTOBUF')
+    AS SELECT
+        entity->trip_update->vehicle->id as vehicle_id,
+        entity->trip_update->trip->trip_id as trip_id,
+        entity->trip_update->trip->direction_id as direction_id,
+        EXPLODE(entity->trip_update->stop_time_update) as stop_time_update,
+        entity->trip_update->timestamp as ts
+    FROM TripEntitiesExploded
+EMIT CHANGES;
