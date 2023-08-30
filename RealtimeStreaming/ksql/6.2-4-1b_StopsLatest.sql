1124fa31e5c4fc64193a76049d6740a9fdc71426
Akira Madono <amadono@resourcedata.com>
2023-08-30 12:24:39 -0700

    key by stop coords and keep latest only

--- RealtimeStreaming/ksql/6.2-4-1b_StopsLatest.sql
+++ RealtimeStreaming/ksql/6.2-4-1b_StopsLatest.sql
@@ -3,16 +3,16 @@ CREATE TABLE StopsLatest
     AS
     SELECT
         stop_coords,
-        LATEST_BY_OFFSET(route_id,3) as route_ids,
-        LATEST_BY_OFFSET(direction_id,3) as direction_ids,
-        LATEST_BY_OFFSET(stop_sequence,3) as stop_sequences,
+        LATEST_BY_OFFSET(route_id) as route_id,
+        LATEST_BY_OFFSET(direction_id) as direction_id,
+        LATEST_BY_OFFSET(stop_sequence) as stop_sequence,
         LATEST_BY_OFFSET(stop_lat) as stop_lat,
         LATEST_BY_OFFSET(stop_lon) as stop_lon,
         LATEST_BY_OFFSET(stop_name) as stop_name,
-        LATEST_BY_OFFSET(vehicle_id,3) as vehicle_ids,
-        LATEST_BY_OFFSET(trip_id,3) as trip_ids,
-        LATEST_BY_OFFSET(stop_time_update,3) as stop_time_updates,
-        LATEST_BY_OFFSET(ts,3) as timestamps
+        LATEST_BY_OFFSET(vehicle_id) as vehicle_id,
+        LATEST_BY_OFFSET(trip_id) as trip_id,
+        LATEST_BY_OFFSET(stop_time_update) as stop_time_update,
+        LATEST_BY_OFFSET(ts) as ts
     FROM StopEventsAnnotated
     GROUP BY stop_coords 
 EMIT CHANGES;
