f994c0f8be85ebf24b2910616c6cb98dfa51403d
Akira Madono <amadono@resourcedata.com>
2023-08-31 11:58:45 -0700

    test out new stop joins

--- RealtimeStreaming/py/gtfs_script.py
+++ RealtimeStreaming/py/gtfs_script.py
@@ -10,9 +10,12 @@ stop_times = pd.read_csv("gtfs/stop_times.txt", dtype=str, keep_default_na=False
 stop_times =stop_times[["trip_id","stop_id","stop_sequence"]]
 
 stops_exp = pd.merge(stops, stop_times, on=["stop_id"])
-all_info = pd.merge(stops_exp, trips, on=["trip_id"])
-all_info.drop(['trip_id'],axis=1, inplace=True)
-all_info.drop_duplicates(keep='first', inplace=True)
-for index,row in all_info.iterrows():
-    print(row["route_id"]+"_"+row["stop_sequence"]+"_"+row["direction_id"]+"\t"+row.to_json())
+df = pd.merge(stops_exp, trips, on=["trip_id"])
+df.drop(columns=['trip_id'], inplace=True)
+df.drop_duplicates(keep='first', inplace=True)
+df = df.reindex(sorted(df.columns), axis=1)
 
+for index,row in df.iterrows():
+    stop_id=row["stop_id"]
+    row.drop(labels=["stop_id"], inplace=True)
+    print(stop_id+"\t"+row.to_json())
