c800f4e8dc1fa2d8f28614b00a046f331ea12f2b
Akira Madono <amadono@resourcedata.com>
2023-08-31 11:58:45 -0700

    test out new stop joins

--- RealtimeStreaming/ksql/5.2-4-1b_StopEventsAnnotated.sql
+++ RealtimeStreaming/ksql/5.2-4-1b_StopEventsAnnotated.sql
@@ -2,20 +2,14 @@ CREATE STREAM StopEventsAnnotated
     WITH (KAFKA_TOPIC='StopEventsAnnotated', VALUE_FORMAT='PROTOBUF')
     AS
     SELECT
-        CONCAT(v.stop_lat,'_',v.stop_lon) as stop_coords,
-        v.k1 as k1,
-        v.k2 as k2,
-        v.route_id as route_id,
-        v.direction_id as direction_id,
-        v.stop_sequence as stop_sequence,
         v.stop_id as stop_id,
+        t.trip_seq_id as trip_seq_id,
+        t.vehicle_id as vehicle_id,
         v.stop_lat as stop_lat,
         v.stop_lon as stop_lon,
         v.stop_name as stop_name,
-        v.vehicle_id as vehicle_id,
-        v.trip_id as trip_id,
         t.stop_time_update as stop_time_update,
         t.ts as ts
     FROM VehicleStopConnectorExp v
-    INNER JOIN TripsKeyed t WITHIN 15 MINUTES ON t.k2 = v.k2
+    JOIN TripsKeyed t WITHIN 15 MINUTES ON t.trip_seq_id = v.trip_seq_id
 EMIT CHANGES;
