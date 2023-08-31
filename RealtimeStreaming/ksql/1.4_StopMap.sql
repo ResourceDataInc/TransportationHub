76cebaa34bdc168700d3a211bf408979d5c8e7da
Akira Madono <amadono@resourcedata.com>
2023-08-31 11:58:45 -0700

    test out new stop joins

--- RealtimeStreaming/ksql/1.4_StopMap.sql
+++ RealtimeStreaming/ksql/1.4_StopMap.sql
@@ -1,12 +1,11 @@
 CREATE TABLE StopMapState (
-id STRING PRIMARY KEY,
-stop_id STRING,
-stop_name STRING,
+stop_id STRING PRIMARY KEY,
+direction_id STRING,
+route_id STRING,
 stop_lat STRING,
 stop_lon STRING,
-stop_sequence STRING,
-route_id STRING,
-direction_id STRING
+stop_name STRING,
+stop_sequence STRING
 )
 WITH (
     KAFKA_TOPIC = 'StopMap', KEY_FORMAT='KAFKA', VALUE_FORMAT='JSON'
