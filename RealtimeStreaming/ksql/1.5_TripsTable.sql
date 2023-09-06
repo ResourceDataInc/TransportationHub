1e9cb8c8757e8ba1042720ff9f4f3f40b63283d2
Akira Madono <amadono@resourcedata.com>
2023-09-06 15:40:55 -0700

    added route information

---
+++ RealtimeStreaming/ksql/1.5_TripsTable.sql
@@ -0,0 +1,10 @@
+CREATE TABLE TripsTable (
+index STRING PRIMARY KEY,
+route_id STRING,
+service_id STRING,
+direction_id INT,
+shape_id BIGINT
+)
+WITH (
+    KAFKA_TOPIC = 'Trips', KEY_FORMAT='KAFKA', VALUE_FORMAT='JSON'
+);
