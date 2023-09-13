1fb3bd9c9a1a387633a605f32c31561bf76fd900
Brandon Kirlin <bkirlin@resourcedata.com>
2023-09-13 21:03:00 +0000

    latest updates to schema

--- models/hub-etl/vehicle_positions.sql
+++ models/hub-etl/vehicle_positions.sql
@@ -1,15 +1,16 @@
 select distinct
-    to_timestamp(v.value:vehicle:timestamp) as timestamp
-    ,v.value:id::number(38,0) as vehicle_id
-    ,v.value:vehicle:position:latitude::number(15,12) as latitude
-    ,v.value:vehicle:position:longitude::number(15,12) as longitude
-    ,v.value:vehicle:position:bearing::number(38,0) as bearing
-    ,v.value:vehicle:position:speed::number(38,0) as speed
-    ,v.value:vehicle:trip:trip_id::number(38,0) as trip_id
-    ,v.value:vehicle:trip:route_id::number(38,0) as route_id
-    ,case when v.value:vehicle:stop_id = '' THEN NULL
-     when v.value:vehicle:stop_id != '' THEN v.value:vehicle:stop_id::number(38,0) 
+    to_timestamp(v.value:VEHICLE:TIMESTAMP) as timestamp
+    ,v.value:ID::number(38,0) as vehicle_id
+    ,v.value:VEHICLE:POSITION:LATITUDE::number(15,12) as latitude
+    ,v.value:VEHICLE:POSITION:LONGITUDE::number(15,12) as longitude
+    ,v.value:VEHICLE:POSITION:BEARING::number(38,0) as bearing
+    ,v.value:VEHICLE:POSITION:SPEED::number(38,0) as speed
+    ,v.value:VEHICLE:TRIP:TRIP_ID::number(38,0) as trip_id
+    ,v.value:VEHICLE:TRIP:ROUTE_ID::number(38,0) as route_id
+    ,case when v.value:VEHICLE:STOP_ID = '' THEN NULL
+     when v.value:VEHICLE:STOP_ID != '' THEN v.value:VEHICLE:STOP_ID::number(38,0) 
      END as stop_id
-    ,v.value:vehicle:current_status::varchar as current_status
-from staging.vehiclepositions vp
-,lateral flatten(input => vp.record_content:entity) v
+    ,v.value:VEHICLE:CURRENT_STATUS::varchar as current_status
+from staging.VEHICLEENTITIESEXPLODED vp
+,lateral flatten(input => vp.record_content) v
+where trip_id is not null
