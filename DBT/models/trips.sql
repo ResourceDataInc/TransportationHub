1de22dc8ce2f92501bf20ce5d63e7d5fb56f9ed2
jrgarrar <jgarrard@resourcedata.com>
2023-09-18 12:20:31 -0600

    Configures DBT for use with the existing pipeline

---
+++ DBT/models/trips.sql
@@ -0,0 +1,9 @@
+select distinct
+    v.value:VEHICLE:TRIP:TRIP_ID::number(38,0) as trip_id
+    ,v.value:VEHICLE:TRIP:ROUTE_ID::number(38,0) as route_id
+    ,case when v.value:VEHICLE:TRIP:DIRECTION_ID::number(38,0) = 0 then 'Outbound'
+         when v.value:VEHICLE:TRIP:DIRECTION_ID::number(38,0) = 1 then 'Inbound'
+    end as route_direction
+from {{ source('transport_hub', 'VEHICLEENTITIESEXPLODED') }} vp
+,lateral flatten(input => vp.record_content) v
+where trip_id is not null
