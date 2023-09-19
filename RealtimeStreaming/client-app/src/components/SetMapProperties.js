bcbe4c206ced9341b79f8a6e3931a65f3bc5aabb
GregBlachly <gblachly@resourcedata.com>
2023-09-19 12:12:11 -0700

    Add api call/query for stops within map bounds

---
+++ RealtimeStreaming/client-app/src/components/SetMapProperties.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useMapEvents } from 'react-leaflet';
+import { getStopsWithinMapBounds } from '../store/stops/stopsActions';
+
+export const SetMapProperties = () => {
+    const [northBound, setNorthBound] = useState();
+    const [eastBound, setEastBound] = useState();
+    const [southBound, setSouthBound] = useState();
+    const [westBound, setWestBound] = useState();
+    const [zoom, setZoom] = useState(14);
+    const dispatch = useDispatch();
+
+     const setMapBounds = (newMapBounds) => {
+         const newNorthBound = newMapBounds._northEast.lat;
+         setNorthBound(newNorthBound);
+         
+         const newEastBound = newMapBounds._northEast.lng;
+         setEastBound(newEastBound);
+         
+         const newSouthBound = newMapBounds._southWest.lat;
+         setSouthBound(newSouthBound); 
+         
+         const newWestBound = newMapBounds._southWest.lng;
+         setWestBound(newWestBound);
+     };
+
+    const map = useMapEvents({
+        move() {
+            const newMapBounds = map.getBounds();
+            setMapBounds(newMapBounds);
+        },
+
+        /*
+        zoom() {
+            const newMapBounds = map.getBounds();
+            setMapBounds(newMapBounds);
+        
+            const newZoom = map.getZoom();
+            setZoom(newZoom)
+        },
+
+        load() {
+            const newMapBounds = map.getBounds();
+            setMapBounds(newMapBounds);
+        },
+        */
+    });
+    
+    useEffect(() => {
+        const stopsChangeInterval = setInterval(() => {
+            const bounds = {
+                north: northBound, 
+                east: eastBound, 
+                south: southBound, 
+                west: westBound,
+            };
+
+            dispatch(getStopsWithinMapBounds(bounds));
+        }, 1000);
+
+        return () => clearInterval(stopsChangeInterval);
+    }, [northBound, eastBound, southBound, westBound]); 
+
+    return (
+        <></>
+    )
+};
