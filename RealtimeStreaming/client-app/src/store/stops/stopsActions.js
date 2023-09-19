822f8e0e74a5d8200d897069c872c150b9ebfbd4
GregBlachly <gblachly@resourcedata.com>
2023-09-19 14:05:12 -0700

    Minor client-app edits

--- RealtimeStreaming/client-app/src/store/stops/stopsActions.js
+++ RealtimeStreaming/client-app/src/store/stops/stopsActions.js
@@ -4,7 +4,7 @@ import { StopsApi } from "../../api/StopsApi";
 export const getAllStops = createAsyncThunk(
     'stops/getAllStops',
     async () => {
-        const api = new StopsApi;
+        const api = new StopsApi();
         const data = await api.getAllStops();
         return data;
     }
@@ -13,7 +13,7 @@ export const getAllStops = createAsyncThunk(
 export const getStopsWithinMapBounds = createAsyncThunk(
     'stops/getStopsWithinMapBounds',
     async (bounds) => {
-        const api = new StopsApi;
+        const api = new StopsApi();
         const data = await api.getStopsWithinMapBounds(bounds.north, bounds.east, bounds.south, bounds.west);
         return data;
     }
