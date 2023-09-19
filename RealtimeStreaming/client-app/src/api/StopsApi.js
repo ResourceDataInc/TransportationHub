5e552358a7ad2066bbed95c3c8c233cc444efbc1
GregBlachly <gblachly@resourcedata.com>
2023-09-19 14:05:12 -0700

    Minor client-app edits

--- RealtimeStreaming/client-app/src/api/StopsApi.js
+++ RealtimeStreaming/client-app/src/api/StopsApi.js
@@ -25,13 +25,15 @@ export class StopsApi {
     }
 
     async getStopsWithinMapBounds(north, east, south, west) {
+        const buffer = 0.025;
+        
         const response = await fetch(`${this.#root}`, {
             method: 'POST',
             headers: {
                 'Accept': 'application/vnd.ksql.v1+json',
             },
             body: JSON.stringify({
-                "ksql": `SELECT * FROM STOPSLATEST WHERE STOP_LAT > ${south} AND STOP_LAT < ${north} AND STOP_LON > ${west} AND STOP_LON < ${east};`,
+                "ksql": `SELECT * FROM STOPSLATEST WHERE STOP_LAT > ${south - buffer} AND STOP_LAT < ${north + buffer} AND STOP_LON > ${west - buffer} AND STOP_LON < ${east + buffer};`,
                 "streamsProperties": {}
             }),
         });
