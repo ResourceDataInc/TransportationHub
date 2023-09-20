4255cc93e53502399ea423c3aeb47c4e4dd294b6
Ubuntu <ubuntu@ip-172-31-15-40.us-west-2.compute.internal>
2023-09-20 07:12:09 +0000

    keep disk from filling up

--- DBT/analyses/athena_on_time_performance.sql
+++ DBT/analyses/athena_on_time_performance.sql
@@ -1,31 +1,25 @@
 SELECT 
-    sum(d.otd)/cast(count(d.otd) as decimal(7,2)) as otd_frac,
-    sum(d.otd) as otd_sum,
-    count(d.otd) as otd_cnt,
-    t.route_id,
-    t.direction_id
+	sum(d.otd)/cast(count(d.otd) as decimal(7,2)) as otd_frac,
+	sum(d.otd) as otd_sum,
+	count(d.otd) as otd_cnt,
+	t.route_id
 FROM (
-    SELECT 
-        trip_id,
-        trip_stop_sequence,
-        CASE WHEN avg(delay) < -60 THEN 0 ELSE 1 END AS otd
-    FROM 
-        DEV.HUB.TRIP_DELAYS
-    WHERE 
-        timestamp IS NOT NULL AND YEAR(timestamp)='2023' AND MONTH(timestamp)='09' AND DAY(timestamp)='14'
-    GROUP BY 
-        trip_id, 
-        trip_stop_sequence
-        ) as d
+	SELECT 
+	trip_id,
+	stop_time_update.stop_sequence,
+	if(avg(stop_time_update.departure.delay) < -60 OR avg(stop_time_update.departure.delay) > 300, 0, 1) as otd
+		FROM "gtfs-realtime"."tripentitiesexplodedstopsexploded"
+		WHERE stop_time_update.departure IS NOT NULL and year='2023' and month='09' and day='17'
+		GROUP BY 
+		trip_id, 
+		stop_time_update.stop_sequence
+) d
 JOIN (
-    SELECT 
-        route_id, 
-        cast(trip_id as varchar) as trip_id,
-        route_direction as direction_id
-    FROM 
-        DEV.HUB.TRIPS
-    ) as t
-ON 
-    d.trip_id = t.trip_id
-GROUP BY
-    t.route_id, t.direction_id;
+	SELECT 
+	route_id, 
+	cast(trip_id as varchar) as trip_id
+	FROM "gtfs-realtime"."trips"
+	WHERE year='2023' AND month='09' AND day='17'
+) t
+ON d.trip_id = t.trip_id
+GROUP BY t.route_id;
