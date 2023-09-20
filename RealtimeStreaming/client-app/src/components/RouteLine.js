e7f7f57fcb02fe4fc1f6d252f63753efd79c145a
GregBlachly <gblachly@resourcedata.com>
2023-09-20 11:20:17 -0700

    Add ability to clear route data

--- RealtimeStreaming/client-app/src/components/RouteLine.js
+++ RealtimeStreaming/client-app/src/components/RouteLine.js
@@ -1,12 +1,17 @@
 import { useSelector } from 'react-redux';
 import { Polyline } from 'react-leaflet';
-import { selectSelectedRoutePositions } from '../store/routes/routesSlice';
+import { selectSelectedRouteId, selectSelectedRoutePositions } from '../store/routes/routesSlice';
 
 export const RouteLine = () => {
+    const selectedRouteId = useSelector(selectSelectedRouteId);
     const selectedRoutePositions = useSelector(selectSelectedRoutePositions);
 
     const pathOptions = {
         color: 'red',
+    };
+
+    if (selectedRouteId === 0 || selectedRouteId === null) {
+        return;
     };
 
     return (
