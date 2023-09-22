26895d172fa3ee700e5d0fd404b23c7f15377674
Akira Madono <amadono@resourcedata.com>
2023-09-22 16:41:44 -0700

    further cleaning

--- RealtimeStreaming/src/main/java/com/resourcedata/transportationhub/realtime/RequestParams.java
+++ RealtimeStreaming/src/main/java/com/resourcedata/transportationhub/realtime/RequestParams.java
@@ -1,28 +1,13 @@
 package com.resourcedata.transportationhub.realtime;
 
 public class RequestParams {
-    public final String baseUrl;
-    public final String ext;
-    public final String name;
-    public final String dataClass;
-    public final int waitTimeMs;
-    public final boolean fileWriteRequested;
-    public final int numLoops;
-    public final String link;
-    public RequestParams(String baseUrl,
-                         String ext,
-                         String name,
-                         String dataClass,
-                         int waitTimeMs,
-                         boolean fileWriteRequested,
-                         int numLoops) {
-        this.baseUrl = baseUrl;
-        this.ext = ext;
-        this.name = name;
-        this.dataClass = dataClass;
-        this.link = baseUrl+"/"+ext+"/"+name;
-        this.fileWriteRequested = fileWriteRequested;
-        this.waitTimeMs = waitTimeMs;
-        this.numLoops = numLoops;
-    }
+    public String baseUrl;
+    public String ext;
+    public String name;
+    public String dataClass;
+    public int waitTimeMs;
+    public boolean fileWriteRequested;
+    public int numLoops;
+    public String link;
+    public void makeLink(){ this.link = baseUrl+"/"+ext+"/"+name; }
 }
