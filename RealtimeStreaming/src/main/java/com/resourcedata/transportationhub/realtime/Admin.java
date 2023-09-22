e42bae2a53ad278d1cb9a1b2f83e9d1557880f60
Akira Madono <amadono@resourcedata.com>
2023-09-22 16:19:42 -0700

    slightly cleaner functions

--- RealtimeStreaming/src/main/java/com/resourcedata/transportationhub/realtime/Admin.java
+++ RealtimeStreaming/src/main/java/com/resourcedata/transportationhub/realtime/Admin.java
@@ -1,9 +1,15 @@
 package com.resourcedata.transportationhub.realtime;
 
+import io.confluent.kafka.serializers.json.KafkaJsonSchemaSerializer;
+import io.confluent.kafka.serializers.protobuf.KafkaProtobufSerializer;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.ListTopicsResult;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
 
@@ -26,5 +32,22 @@ public class Admin {
         } catch (ExecutionException | InterruptedException e) {
             e.printStackTrace(System.err);
         }
+    }
+    public static Properties buildProperties(RequestParams requestParams){
+        Properties properties = new Properties();
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        try(InputStream input = classLoader.getResourceAsStream("producer.properties")){
+            properties.load(input);
+        } catch (IOException ioError){
+            ioError.printStackTrace(System.err);
+            System.exit(1);
+        }
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        if(requestParams.dataClass.equals("GtfsRealtime")) {
+            properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaProtobufSerializer.class);
+        } else {
+            properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaJsonSchemaSerializer.class);
+        }
+        return properties;
     }
 }
