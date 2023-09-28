6172c264735b8fcefb8eefc33e7877b49798cc58
Rick Busch <rbusch@resdat.com>
2023-09-27 23:39:44 -0700

    added views and first bi report

---
+++ Databases/TRANSPORTATION_HUB/HUB/Views/ON_TIME_PERFORMANCE.sql
@@ -0,0 +1,43 @@
+create or replace view TRANSPORTATION_HUB.HUB.ON_TIME_PERFORMANCE(
+	OTD_FRAC,
+	OTD_SUM,
+	OTD_CNT,
+	ROUTE_ID
+) as
+SELECT 
+    sum(d.otd)/cast(count(d.otd) as decimal(7,2)) as otd_frac,
+    sum(d.otd) as otd_sum,
+    count(d.otd) as otd_cnt,
+    t.route_id
+FROM (
+        SELECT 
+            trip_id,
+            stop_sequence,
+            CASE WHEN avg(delay) < -60 OR avg(delay) > 300 THEN 0 ELSE 1 
+            END AS otd
+        FROM 
+            (SELECT 
+            record_content:TRIP_ID::INTEGER as trip_id,
+            record_content:STOP_TIME_UPDATE.STOP_SEQUENCE as stop_sequence,
+            record_content:STOP_TIME_UPDATE.DEPARTURE as departure,
+            record_content:STOP_TIME_UPDATE.DEPARTURE.DELAY as delay,
+            to_timestamp_ntz(record_content:TS) as eventtime
+            FROM TRANSPORTATION_HUB.STAGING.TRIPENTITIESEXPLODEDSTOPSEXPLODED  
+            ) a
+        WHERE
+            departure !='' AND YEAR(eventtime) = 2023 AND MONTH(eventtime)= 9 AND DAY(eventtime)= 17
+        GROUP BY
+            trip_id,
+            stop_sequence
+) as d
+JOIN (
+        SELECT
+            route_id,
+            trip_id as trip_id
+        FROM
+            DEV.HUB.TRIPS
+) as t
+ON
+    d.trip_id = t.trip_id
+GROUP BY
+    t.route_id;
