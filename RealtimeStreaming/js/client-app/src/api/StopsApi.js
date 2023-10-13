e5a7868309483240fa89825a090b9227d8841b62
Akira Madono <amadono@resourcedata.com>
2023-10-12 17:09:49 -0700

    add default null

--- RealtimeStreaming/js/client-app/src/api/StopsApi.js
+++ RealtimeStreaming/js/client-app/src/api/StopsApi.js
@@ -1,46 +1,18 @@
+import sqlQuery  from './GeneralApi';
+
 export class StopsApi {
     constructor() {}
 
-    #root = 'http://localhost:8088/query';
-    
     async getAllStops(request) {
-        const response = await fetch(`${this.#root}`, {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/vnd.ksql.v1+json',
-            },
-            body: JSON.stringify({
-                "ksql": `SELECT * FROM STOPSLATEST WHERE ROUTE_ID = '${request.routeId}' AND DIRECTION_ID = ${request.directionId};`,
-                "streamsProperties": {}
-            }),
-        });
-
-        const json = await response.json();
-        const header = json.shift();
-
-        return json;
+        let ksql;
+        if(!request.routeId || !request.directionId) ksql = 'SELECT * FROM STOPSLATEST;';
+        else ksql = `SELECT * FROM STOPSLATEST WHERE ROUTE_ID = '${request.routeId}' AND DIRECTION_ID = ${request.directionId};`;
+        return sqlQuery(ksql);
     }
 
     async getStopsWithinMapBounds(north, east, south, west) {
         const buffer = 0.025;
-        
-        const response = await fetch(`${this.#root}`, {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/vnd.ksql.v1+json',
-            },
-            body: JSON.stringify({
-                "ksql": `SELECT * FROM STOPSLATEST WHERE STOP_LAT > ${south - buffer} AND STOP_LAT < ${north + buffer} AND STOP_LON > ${west - buffer} AND STOP_LON < ${east + buffer};`,
-                "streamsProperties": {}
-            }),
-        });
-
-        const json = await response.json();
-        console.log(json);
-
-        const header = json.shift();
-        //console.log(header);
-        
-        return json;
+        const ksql = `SELECT * FROM STOPSLATEST WHERE STOP_LAT > ${south - buffer} AND STOP_LAT < ${north + buffer} AND STOP_LON > ${west - buffer} AND STOP_LON < ${east + buffer};`;
+        return sqlQuery(ksql);
     }
 };
--- RealtimeStreaming/js/client-app/src/containers/Map.js
+++ RealtimeStreaming/js/client-app/src/containers/Map.js
@@ -82,7 +82,7 @@ export const Map = () => {
                         )
                     })}
 
-                    <RouteLine/>
+                    <RouteLine />
                     <OfficeMarker/>
                 </MapContainer>
             </div>
