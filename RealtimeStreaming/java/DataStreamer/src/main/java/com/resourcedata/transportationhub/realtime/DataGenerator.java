98fc0743cccf72f370dbde6903993c6d66a76f4b
Akira Madono <amadono@resourcedata.com>
2023-10-18 11:45:14 -0700

    unit tests expanded

--- RealtimeStreaming/java/DataStreamer/src/main/java/com/resourcedata/transportationhub/realtime/DataGenerator.java
+++ RealtimeStreaming/java/DataStreamer/src/main/java/com/resourcedata/transportationhub/realtime/DataGenerator.java
@@ -1,6 +1,7 @@
 package com.resourcedata.transportationhub.realtime;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.transit.realtime.GtfsRealtime.FeedMessage;
 import com.google.transit.realtime.ResultSetAlert;
 import com.google.transit.realtime.ResultSetVehicle;
@@ -25,18 +26,21 @@ import java.util.LinkedList;
 import java.util.Map;
 import java.util.Objects;
 
-public class DataGenerator {
+public class DataGenerator implements AutoCloseable {
     private final GtfsStreamer cli;
     private int numLoops;
     private final HttpGet request;
     private final LinkedList<Route> existingRoutes;
+    public CloseableHttpClient closeableHttpClient;
     private final ObjectMapper objectMapper = new ObjectMapper();
     public DataGenerator(GtfsStreamer cli){
         this.cli = cli;
         this.numLoops = cli.numLoops;
         this.request = setupRequest();
         this.existingRoutes = new LinkedList<>();
+        this.closeableHttpClient = HttpClients.createDefault();
     }
+    public HttpGet getRequest(){ return request; }
     private HttpGet setupRequest(){
         HttpGet httpGet = new HttpGet(cli.url.link);
         try {
@@ -57,55 +61,61 @@ public class DataGenerator {
         else httpGet.setHeader("Content-Type", "application/json");
         return httpGet;
     }
-    static class UnsuccessfulHttpRequestException extends RuntimeException {
-        public UnsuccessfulHttpRequestException(String errorMessage){
+    static class NoMoreDataException extends RuntimeException {
+        public NoMoreDataException(String errorMessage){
             super(errorMessage);
         }
     }
-    private byte[] getHttpResponse() throws IOException {
+    public byte[] getHttpResponse(){
         byte[] result = null;
-        try (CloseableHttpClient client = HttpClients.createDefault()) {
-            CloseableHttpResponse response = client.execute(this.request);
-            StatusLine statusLine = response.getStatusLine();
-            final int statusCode = statusLine.getStatusCode();
-            if(statusCode != HttpStatus.SC_OK) throw new UnsuccessfulHttpRequestException("request not successful with code: " + statusCode);
-            result = EntityUtils.toByteArray(response.getEntity());
+        try (CloseableHttpResponse response = this.closeableHttpClient.execute(this.request)) {
+            if (response != null){
+                StatusLine statusLine = response.getStatusLine();
+                final int statusCode = statusLine.getStatusCode();
+                if(statusCode == HttpStatus.SC_OK) result = EntityUtils.toByteArray(response.getEntity());
+            }
             return result;
-        }
-        catch (UnsuccessfulHttpRequestException r) {
-            r.printStackTrace(System.err);
-            System.exit(1);
         }
         catch (ConnectionClosedException err){
             err.printStackTrace(System.err);
+            this.closeableHttpClient = HttpClients.createDefault();
+        }
+        catch (IOException e){
+            e.printStackTrace(System.err);
         }
         return result;
     }
-    private byte[] processResponse(){
+
+    @Override
+    public void close() throws Exception {
+        this.closeableHttpClient.close();
+    }
+    private byte[] processResponse() {
         byte[] response = null;
-        try {
-            if(cli.numLoops == -1 || this.numLoops > 0) {
-                response = getHttpResponse();
+        if(cli.numLoops == -1 || this.numLoops > 0) {
+            response = getHttpResponse();
+            if (response == null) return null;
+            try {
                 if (cli.fileWriteRequested)
-                    FileUtils.writeByteArrayToFile(new File("gtfs-rt-" + cli.url.service+ "-"+ this.numLoops +".bin"), response);
+                    FileUtils.writeByteArrayToFile(new File("gtfs-rt-" + cli.url.service + "-" + this.numLoops + ".bin"), response);
                 Thread.sleep(cli.waitTimeMs);
-                if(cli.numLoops != -1) this.numLoops--;
-            } else {
-                System.exit(0);
             }
-        }
-        catch (Exception e){
-            e.printStackTrace(System.err);
+            catch (IOException | InterruptedException e){
+                e.printStackTrace(System.err);
+            }
+            if(cli.numLoops != -1) this.numLoops--;
+        } else {
+            throw new NoMoreDataException("started out with: "+cli.numLoops+" loops");
         }
         return response;
     }
-    public FeedMessage generateProto(){
+    public FeedMessage generateProto() {
         FeedMessage message = null;
         try {
             byte[] response = processResponse();
+            if (response == null) return null;
             message = FeedMessage.parseFrom(response);
-        }
-        catch (Exception e){
+        } catch (InvalidProtocolBufferException e){
             e.printStackTrace(System.err);
         }
         return message;
@@ -114,35 +124,37 @@ public class DataGenerator {
         ResultSetVehicle resultSetVehicle = null;
         try {
             byte[] response = processResponse();
+            if (response == null) return null;
             resultSetVehicle = objectMapper.readValue(response, ResultSetVehicle.class);
-        }
-        catch(Exception e) {
+        } catch (IOException e){
             e.printStackTrace(System.err);
         }
         return resultSetVehicle;
     }
     public ResultSetAlert generateResultSetAlert() {
         ResultSetAlert resultSetAlert = null;
-        try {
+        try{
             byte[] response = processResponse();
+            if (response == null) return null;
             resultSetAlert = objectMapper.readValue(response, ResultSetAlert.class);
-        }
-        catch(Exception e) {
+        } catch (IOException e){
             e.printStackTrace(System.err);
         }
         return resultSetAlert;
     }
 
-    public Route generateRoute(){
+    public Route generateRoute() {
         Route route = null;
         if(existingRoutes.isEmpty()) {
+            byte[] response = processResponse();
+            if (response == null) return null;
             try {
-                byte[] response = processResponse();
                 ResultSetRoute resultSetRoute = objectMapper.readValue(response, ResultSetRoute.class);
                 existingRoutes.addAll(resultSetRoute.resultSet.route);
                 route = existingRoutes.pop();
-            } catch (Exception e) {
-                e.printStackTrace(System.err);
+            }
+            catch (IOException e){
+               e.printStackTrace(System.err);
             }
         }
         else route = existingRoutes.pop();
